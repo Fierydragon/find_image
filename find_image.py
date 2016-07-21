2fb38b54906f19e7f32ca3adcdecbfc3cbce1389
LC <lichen1988913@qq.com>
2016-07-21 16:35:28 +0800

    add ispolygon()
    Add ispolygon() function, and find image can work correctly.

--- find_image.py
+++ find_image.py
@@ -79,21 +79,21 @@ def filter_matches(kp1, kp2, matches, ratio = 0.75):
     return p1, p2, kp_pairs
 
 def match_image(image, matcher, kp_and_dest_pairs):
-	# index:a sequence of descriptors
 	# image:the image which is compared to match
-	# kp_and_dest_pairs: the pairs of keypoints and descriptors
+	# kp_and_dest_pairs: the pairs of keypoints, descriptors, iamge sizes and fileName
 	# Get image descriptors
 	image = get_image(image)
 	keypoints, descriptors = get_image_features(image)
 
 	h2, w2 = image.shape[:2]
-	#corners = numpy.float32([[0, 0], [w1, 0], [w1, h1], [0, h1]])
 
 	for kpd in kp_and_dest_pairs:
 		imgSize = kpd[2]
 		h1 = numpy.int(imgSize[0])
 		w1 = numpy.int(imgSize[1])
-		h3 = imgSize[0]
+		# h2 = imgSize[0]
+		# w2 = imgSize[1]
+		#
 		corners = numpy.float32([[0, 0], [w1, 0], [w1, h1], [0, h1]])
 
 
@@ -114,18 +114,51 @@ def match_image(image, matcher, kp_and_dest_pairs):
 		if H is not None:
 			corners = corners.reshape(1, -1, 2)
 			print  "corners : ", corners
-			#obj_corners = numpy.int32(cv2.perspectiveTransform(corners.reshape(1, -1, 2), H).reshape(-1, 2) + (w1, 0))
 
 			obj_corners = numpy.int32(cv2.perspectiveTransform(corners,H).reshape(-1,2))
 			print "obj = " , obj_corners
 
 			#corners = numpy .int32(cv2.perspectiveTransform(corners.reshape(1, -1, 2), H).reshape(-1, 2) + (w1, 0))
 
+
+			# ================= just show the query image =================
 			vis = numpy.zeros((h2, w2), numpy.uint8)
 			vis[:h2, :w2] = image
 			vis = cv2.cvtColor(vis, cv2.COLOR_GRAY2BGR)
-
 			cv2.polylines(vis, [obj_corners], True, (0, 0, 255))
+
+			# =================== test for cv2.polylines ===================
+            # p1 = [100, 100]
+            # p2 = [100, 200]
+            # p3 = [200, 200]
+            # p4 = [200, 100]
+            #
+            # points = numpy.int32([p1, p2, p3, p4])
+            #
+            # red = (0 , 0 , 255)
+            # green = (0, 255, 0)
+            # blue = (255, 0, 0)
+            # sky_blue = (255, 255, 0)
+            #
+            # color = [red, green, blue, sky_blue]
+            #
+            # #col = red
+            # r = 2
+            # thickness = 3
+            #
+            # for (x1,y1), col in zip(points,color):
+				# cv2.line(vis, (x1 - r, y1 - r), (x1 + r, y1 + r), col, thickness)
+				# cv2.line(vis, (x1 - r, y1 + r), (x1 + r, y1 - r), col, thickness)
+            #
+            # cv2.polylines(vis,[points],True,(0,255,255))
+
+
+
+			is_polygon = ispolygon(obj_corners)
+			if is_polygon:
+				print "This image may be matched!"
+			else:
+				print "This image may NOT be matched!"
 			#print math.sqrt((obj_corners[0].x - obj_corners[1].x) * (obj_corners[0].x - obj_corners[1].x) + (
 				#obj_corners[0].y - obj_corners[1].y) * (obj_corners[0].y - obj_corners[1].y))
 			cv2.imshow(kpd[3], vis)
@@ -160,51 +193,111 @@ def match_image(image, matcher, kp_and_dest_pairs):
 	# print "==========="
     #
 	# return matched_image
-def explore_match(win, img1, img2, kp_pairs, status = None, H = None):
-    h1, w1 = img1.shape[:2]
-    h2, w2 = img2.shape[:2]
-    vis = numpy.zeros((max(h1, h2), w1+w2), numpy.uint8)
-    vis[:h1, :w1] = img1
-    vis[:h2, w1:w1+w2] = img2
-    vis = cv2.cvtColor(vis, cv2.COLOR_GRAY2BGR)
+# def explore_match(win, img1, img2, kp_pairs, status = None, H = None):
+#     h1, w1 = img1.shape[:2]
+#     h2, w2 = img2.shape[:2]
+#     vis = numpy.zeros((max(h1, h2), w1+w2), numpy.uint8)
+#     vis[:h1, :w1] = img1
+#     vis[:h2, w1:w1+w2] = img2
+#     vis = cv2.cvtColor(vis, cv2.COLOR_GRAY2BGR)
+#
+#     if H is not None:
+#         corners = numpy.float32([[0, 0], [w1, 0], [w1, h1], [0, h1]])
+#         corners = numpy.int32( cv2.perspectiveTransform(corners.reshape(1, -1, 2), H).reshape(-1, 2) + (w1, 0) )
+#         cv2.polylines(vis, [corners], True, (255, 255, 255))
+#
+#     if status is None:
+#         status = numpy.ones(len(kp_pairs), numpy.bool_)
+#     p1 = numpy.int32([kpp[0].pt for kpp in kp_pairs])
+#     p2 = numpy.int32([kpp[1].pt for kpp in kp_pairs]) + (w1, 0)
+#
+#     green = (0, 255, 0)
+#     red = (0, 0, 255)
+#     white = (255, 255, 255)
+#     kp_color = (51, 103, 236)
+#     for (x1, y1), (x2, y2), inlier in zip(p1, p2, status):
+#         if inlier:
+#             col = green
+#             cv2.circle(vis, (x1, y1), 2, col, -1)
+#             cv2.circle(vis, (x2, y2), 2, col, -1)
+#         else:
+#             col = red
+#             r = 2
+#             thickness = 3
+#             cv2.line(vis, (x1-r, y1-r), (x1+r, y1+r), col, thickness)
+#             cv2.line(vis, (x1-r, y1+r), (x1+r, y1-r), col, thickness)
+#             cv2.line(vis, (x2-r, y2-r), (x2+r, y2+r), col, thickness)
+#             cv2.line(vis, (x2-r, y2+r), (x2+r, y2-r), col, thickness)
+#     vis0 = vis.copy()
+#     for (x1, y1), (x2, y2), inlier in zip(p1, p2, status):
+#         if inlier:
+#             cv2.line(vis, (x1, y1), (x2, y2), green)
+#
+#     cv2.imshow(win, vis)
 
-    if H is not None:
-        corners = numpy.float32([[0, 0], [w1, 0], [w1, h1], [0, h1]])
-        corners = numpy.int32( cv2.perspectiveTransform(corners.reshape(1, -1, 2), H).reshape(-1, 2) + (w1, 0) )
-        cv2.polylines(vis, [corners], True, (255, 255, 255))
+def cosVector(x,y):
+	l=len(x)
+	# len(x)
+	if(len(x)!=len(y)):
+		print 'error input,x and y is not in the same space'
+		return -9
 
-    if status is None:
-        status = numpy.ones(len(kp_pairs), numpy.bool_)
-    p1 = numpy.int32([kpp[0].pt for kpp in kp_pairs])
-    p2 = numpy.int32([kpp[1].pt for kpp in kp_pairs]) + (w1, 0)
+	result1=0.0
+	result2=0.0
+	result3=0.0
 
-    green = (0, 255, 0)
-    red = (0, 0, 255)
-    white = (255, 255, 255)
-    kp_color = (51, 103, 236)
-    for (x1, y1), (x2, y2), inlier in zip(p1, p2, status):
-        if inlier:
-            col = green
-            cv2.circle(vis, (x1, y1), 2, col, -1)
-            cv2.circle(vis, (x2, y2), 2, col, -1)
-        else:
-            col = red
-            r = 2
-            thickness = 3
-            cv2.line(vis, (x1-r, y1-r), (x1+r, y1+r), col, thickness)
-            cv2.line(vis, (x1-r, y1+r), (x1+r, y1-r), col, thickness)
-            cv2.line(vis, (x2-r, y2-r), (x2+r, y2+r), col, thickness)
-            cv2.line(vis, (x2-r, y2+r), (x2+r, y2-r), col, thickness)
-    vis0 = vis.copy()
-    for (x1, y1), (x2, y2), inlier in zip(p1, p2, status):
-        if inlier:
-            cv2.line(vis, (x1, y1), (x2, y2), green)
+	for i in range(l):
+		result1+=x[i]*y[i]	#sum(x * y)
+		result2+=x[i]**2	#sum(x * x)
+		result3+=y[i]**2	#sum(y * y)
 
-    cv2.imshow(win, vis)
+	if result2 == 0 or result3 == 0:
+		return -10
+	else:
+		# print "result1 = ",result1
+		# print "result2 = ",result2
+		# print "result3 = ",result3
+		# print("result is "+str(cosVec))
+		cosVec = result1 / ((result2 * result3) ** 0.5)
+		return cosVec
+
+
+
+def vector(p1,p2):
+
+	if len(p1) == len(p2):
+		v = []
+		for i in range(len(p1)):
+			v.append(p1[i] - p2[i])
+		return v
+	else:
+		print "Error:len(p1) != len(p2)"
+		return
+
+def ispolygon(points):
+	vec1 = vector(points[0],points[1])
+	vec2 = vector(points[0],points[3])
+	vec3 = vector(points[1],points[2])
+	cos1 = cosVector(vec1,vec2)
+	cos2 = cosVector(vec1,vec3)
+	if cos1>-0.1 and cos1<0.1 and cos2>-0.1 and cos2<0.1:
+		print "This area is polygon-like."
+		return True
+	else:
+		return False
 
 if __name__ == "__main__":
 	print "OpenCV Demo, OpenCV version " + cv2.__version__
-	
+
+	# ======= test for ispolygon() =======
+	# p1 = [1,1]
+	# p2 = [200,1]
+	# p3 = [200,200]
+	# p4 = [1,200]
+    #
+	# points = [p1,p2,p3,p4]
+	# ispolygon(points)
+
 	start_time = time.time()
 	flann_matcher,kps,dests,sizes = train_index()  #get matcher with a sequence of desciptors #!!
 	kp_and_dest_pairs = zip(kps,dests,sizes,files)
@@ -227,6 +320,7 @@ if __name__ == "__main__":
 	raw_matches = matcher.knnMatch(desc1, trainDescriptors=desc2, k=2)
 
 	start_time = time.time()
+
 	match_image("t3.jpg", flann_matcher, kp_and_dest_pairs)
 	print "Matching took", (time.time() - start_time), "s."
 
